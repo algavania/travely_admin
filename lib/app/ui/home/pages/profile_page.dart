affcbb100b21687528a17717c5b4667be5693eac
Alga <algavania454@gmail.com>
2022-12-19 04:57:32 +0700

    style: [AV] update profile ui

--- lib/app/ui/home/pages/profile_page.dart
+++ lib/app/ui/home/pages/profile_page.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:loader_overlay/loader_overlay.dart';
-import 'package:travely_admin/app/common/shared_code.dart';
+import 'package:sizer/sizer.dart';
+
+import '../../../common/shared_code.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -10,15 +14,27 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  User? _user;
+
+  @override
+  void initState() {
+    _user = FirebaseAuth.instance.currentUser;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Profil'),),
-      body: Padding(
+      appBar: AppBar(title: const Text('Profil')),
+      body: Container(
         padding: const EdgeInsets.all(SharedCode.defaultPadding),
+        width: double.infinity,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Text(_user?.displayName ?? '', style: GoogleFonts.nunito(fontWeight: FontWeight.bold, fontSize: 14.sp),),
+            Text(_user?.email ?? ''),
+            const Spacer(),
             ElevatedButton(onPressed: () {
               SharedCode.showAlertDialog(context, 'Konfirmasi', 'Apakah kamu yakin ingin keluar?', () {
                 context.loaderOverlay.show();
